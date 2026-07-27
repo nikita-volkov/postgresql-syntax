1ecc6fc43d3ae5d6c4d64df121ec91f73af0d299
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-07-27 03:21:05 +0300

    Fix import alphabetization in Prelude.hs
    Test.QuickCheck must come before Text.Printf (Test < Text alphabetically).

--- library/PostgresqlSyntax/Prelude.hs
+++ library/PostgresqlSyntax/Prelude.hs
@@ -73,9 +73,9 @@ import System.IO.Unsafe as Exports
 import System.Mem as Exports
 import System.Mem.StableName as Exports
 import System.Timeout as Exports
+import Test.QuickCheck as Exports (Arbitrary (..), Gen, choose, elements, listOf, oneof, sized, vectorOf)
 import Text.Printf as Exports (hPrintf, printf)
 import Text.Read as Exports (Read (..), readEither, readMaybe)
-import Test.QuickCheck as Exports (Arbitrary (..), Gen, choose, elements, listOf, oneof, sized, vectorOf)
 import Unsafe.Coerce as Exports
 import Prelude as Exports hiding (all, and, any, concat, concatMap, elem, fail, foldl, foldl1, foldr, foldr1, id, mapM, mapM_, maximum, minimum, notElem, or, product, sequence, sequence_, sum, (.))
 
